a9be4223a05ef7e1b25beef9ad4231bcccf2a725
Хоханов Данил111 <ebefur6@gmail.com>
2026-03-16 13:23:31 +0500

    Add project files.

---
+++ Tyuiu.KhokhanovDA.Sprint2.Task0.V15.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.KhokhanovDA.Sprint2.Task0.V15.Lib;
+namespace Tyuiu.KhokhanovDA.Sprint2.Task0.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 3105;
+            int y = 275;
+            bool[] res =  new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            bool[] wait = new bool[6] {false, false, true, true, false, false};
+
+            CollectionAssert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.KhokhanovDA.Sprint2.Task0.V15/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.KhokhanovDA.Sprint2.Task0.V15.Lib;
+DataService ds = new DataService();
+int x = 3105;
+int y = 275;
+bool[] res = new bool[6];
+res = ds.GetCompareOperations(x, y);
+
+
+Console.Title = "Спринт #2 | Выполнила: Аймурзина Ю. В. | РППб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #2                                                               *");
+Console.WriteLine("* Тема: Операции сравнения                                                *");
+Console.WriteLine("* Задание #0                                                              *");
+Console.WriteLine("* Вариант #15                                                             *");
+Console.WriteLine("* Выполнила: Аймурзина Ю. В. | РППб-25-1                                  *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+Console.WriteLine("* последовательность операций не должна нарушаться)                       *");
+Console.WriteLine("* и арифметических выражений,которая вернет логическую последовательность *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("X = " + x);
+Console.WriteLine("Y = " + y);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+for (int i = 0; i < 6; i++)
+{
+    Console.WriteLine(res[i]);
+}
+Console.ReadKey();
